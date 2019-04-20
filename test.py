8855ae328fd46120e42f58b8c543407b3962869e
vdefontnouvelle <vdefontnouvelle@cs227-592.local>
2019-04-20 15:25:01 -0700

    Golf data and harmonic flow

--- test.py
+++ test.py
@@ -12,4 +12,34 @@ def gradient (vec):
             M[j][i] = -1 * diff
     return M
 
-print(gradient([4,3,1]))
+def makeGradientMatrix (n):
+    M = np.zeros((n*n,n))
+    c1 = 0
+    for i in range(n):
+        c2 = 0
+        for j in range(n):
+            row = n*i + j
+            M[row][c1] -= 1
+            M[row][c2] += 1
+            c2 += 1
+        c1 += 1
+    return M
+
+# Input: a matrix of weights
+def makeDivMatrix (W):
+    n = len(W)
+    M = np.zeros((n,n*n))
+    for row in range(n):
+        for col in range(n):
+            realCol = row + n * col
+            M[row][realCol] = W[row][col]
+    return M
+
+print(makeGradientMatrix(4))
+
+W = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+print(makeDivMatrix(W))
